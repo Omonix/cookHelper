f2119f2b8e4a25234a8844f79b59ead2d5c588f0
Louis Blonde <louis.blonde24@gmail.com>
2025-10-24 18:41:32 +0200

    add data base

--- cookSort.py
+++ cookSort.py
@@ -1,7 +1,18 @@
 import cohere, os, dotenv
+from pymongo import MongoClient
 
 dotenv.load_dotenv()
 AI_KEY = os.getenv('AI_KEY')
+DB_KEY = os.getenv('DB_KEY')
 
-co = cohere.Client(AI_KEY)
+try:
+    co = cohere.Client(AI_KEY)
+    print("Connected to AI ✅")
+except:
+    print("Error 501 : Can't connect to AI API ❌")
+try:
+    client = MongoClient(DB_KEY)
+    print("Connected to Data Base ✅")
+except:
+    print("Error 501 : Can't connect to Data Base ❌")
 #print(co.chat(message='Quel jour sommes nous').text)
