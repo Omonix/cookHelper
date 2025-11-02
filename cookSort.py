f0e9e9e9379803966cab8fe3b1f1f7f3a2874c2a
Louis Blonde <louis.blonde24@gmail.com>
2025-11-02 21:43:00 +0100

    add generator

--- cookSort.py
+++ cookSort.py
@@ -23,7 +23,6 @@ except:
     print("Error 501 : Can't connect to Data Base ❌")
 
 db = client["childs"]
-#user1 = db["user1"]
 
 @app.route("/food", methods=["POST"], strict_slashes=False)
 def food():
@@ -32,6 +31,9 @@ def food():
     new_data = {'food': data.get("food"), 'like': data.get("like")}
     result = user_selected.insert_one(new_data)
     return flask.jsonify({"_id": str(result.inserted_id)}), 201
+@app.route("/generate", methods=["GET"], strict_slashes=False)
+def gen():
+    return co.chat(message=f'Find a recipe (response in {flask.request.args.get("lang")})').text, 201
 
 if __name__ == "__main__":
     try:
@@ -39,4 +41,3 @@ if __name__ == "__main__":
         app.run(host=URL_API, port=5000, debug=True)
     except:
         print("Error 501 : Can't run server ❌")
-#print(co.chat(message='Quel jour sommes nous').text)
