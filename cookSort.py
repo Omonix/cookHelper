f5561820a8aa0241a28ce4a6bf5aa5001191df03
Louis Blonde <louis.blonde24@gmail.com>
2025-11-03 22:45:00 +0100

    add client verify

--- cookSort.py
+++ cookSort.py
@@ -1,4 +1,4 @@
-import cohere, os, dotenv, flask_cors, flask, pymongo
+import cohere, os, dotenv, flask_cors, flask, pymongo, hmac, hashlib, base64, time, json
 
 def to_json(doc):
     doc["_id"] = str(doc["_id"])
@@ -24,16 +24,26 @@ except:
 
 db = client["childs"]
 
-@app.route("/food", methods=["POST"], strict_slashes=False)
-def food():
-    data = flask.request.get_json()
-    user_selected = db[data.get("user")]
-    new_data = {'food': data.get("food"), 'like': data.get("like")}
+def add_food(data):
+    user_selected = db[data["user"]]
+    new_data = {'food': data["food"], 'like': data["like"]}
     result = user_selected.insert_one(new_data)
-    return flask.jsonify({"_id": str(result.inserted_id)}), 201
+    return flask.jsonify({"_id": str(result.inserted_id)})
 @app.route("/generate", methods=["GET"], strict_slashes=False)
 def gen():
     return co.chat(message=f'Find a recipe (response in {flask.request.args.get("lang")})').text, 201
+@app.route("/proxy", methods=["POST"], strict_slashes=False)
+def verify():
+    data = flask.request.get_json()
+    body = "{" + f'"user":{data.get("user")},"food":{data.get("food")},"like":{data.get("like")}' + "}"
+    if time.time() - data.get("time") < 120:
+        signature = hashlib.sha256((os.getenv('APP_SECRET') + body + data.get("time")).encode()).hexdigest()
+        if signature == data.get("code"):
+            return add_food(body), 201
+        else:
+            print("Wrong code")
+    else:
+        print('Later')
 
 if __name__ == "__main__":
     try:
