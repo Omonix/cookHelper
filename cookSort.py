fc134f0b381489e80ce8dfe467d1277fe1109752
Louis Blonde <louis.blonde24@gmail.com>
2025-11-12 21:03:39 +0100

    add docstring

--- cookSort.py
+++ cookSort.py
@@ -1,4 +1,4 @@
-import cohere, os, dotenv, flask_cors, flask, pymongo, hmac, hashlib, base64, time, json
+import cohere, os, dotenv, flask_cors, flask, pymongo, hashlib, time
 
 def to_json(doc):
     doc["_id"] = str(doc["_id"])
@@ -25,6 +25,9 @@ except:
 db = client["childs"]
 
 def is_signed_food(data):
+    """Function that authenticity the call for food.
+        - Input : - data : call's content - dict
+        - Output : return if the call is successfully authenticated - bool"""
     body = "{" + f'"user":"{data.get("user")}","food":"{data.get("food")}","like":{"true" if data.get('like') else "false"}' + "}"
     if time.time() - data.get("time") < 120:
         signature = hashlib.sha256((os.getenv('APP_SECRET') + body + f'{data.get("time")}').encode()).hexdigest()
@@ -37,6 +40,9 @@ def is_signed_food(data):
         print('Later')
         return False
 def is_signed_removed(data):
+    """Function that authenticity the call for child before is will removed.
+        - Input : - data : call's content - dict
+        - Output : return if the call is successfully authenticated - bool"""
     body = "{" + f'"name":"{data.get("name")}"' + "}"
     if time.time() - data.get("time") < 120:
         signature = hashlib.sha256((os.getenv("APP_SECRET") + body + f'{data.get("time")}').encode()).hexdigest()
@@ -51,6 +57,9 @@ def is_signed_removed(data):
 
 @app.route("/generate", methods=["GET"], strict_slashes=False)
 def gen():
+    """Root that generate a recipe with all informations in database.
+        - Input : - lang : the return's language - str
+        - Output : the response of the AI - str"""
     all_disliked = []
     all_liked = []
     collections = db.list_collection_names()
@@ -63,10 +72,12 @@ def gen():
         for e in liked:
             if e["food"] not in all_liked:
                 all_liked.append(e["food"])
-    print(all_liked, all_disliked)
     return co.chat(message=f"Find a recipe for {len(collections)} people (answer in {flask.request.args.get("lang")}, without : {all_disliked} and try to prioritize : {all_liked})").text, 201
 @app.route("/addfood", methods=["POST"], strict_slashes=False)
 def addfood():
+    """Root that add a food in database.
+        - Input : - data : all infos to verify the call and post the food in database - dict
+        - Output : response with message and his code - str, int"""
     data = flask.request.get_json()
     if is_signed_food(data):
         try:
@@ -86,6 +97,9 @@ def addfood():
         return "Can't add ingredient", 401
 @app.route('/removechild', methods=["POST"], strict_slashes=False)
 def removechild():
+    """Root that removed a child from the database.
+        - Input : - data : all informations to verify and delete the child in the database - dict
+        - Ouput : response with a message and his code - str, int"""
     data = flask.request.get_json()
     if is_signed_removed(data):
         if data.get("name") in db.list_collection_names():
